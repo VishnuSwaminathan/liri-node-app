4974845190e843ad116a51c9dd2ab1f427a380c9
VishnuSwaminathan <vishcodes@gmail.com>
2018-08-11 12:48:42 -0400

    secondCommit

---
+++ assets/javascript/liri.js
@@ -0,0 +1,10 @@
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+// COMMANDS
+// * `my-tweets`
+
+//     * `spotify-this-song`
+
+//     * `movie-this`
+
+//     * `do-what-it-says`
